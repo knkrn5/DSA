ea904bfc78dd351dfb69c37b6275bfa3b0dbc22c
Karan <xkaranx5@gmail.com>
2025-03-03 09:09:22 +0530

    1st dsa

--- js/test.js
+++ js/test.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+
+const firstname = "karan";
+
+let word = ''
+
+for (let i = 0; i < firstname.length; i++) {
+    if(i === 0){
+        const temp = firstname[i].toUpperCase();
+        word  +=temp
+    }else{
+        const temp = firstname[i];
+        word += temp
+    }
+    
+}
+
+console.log(word)
+
+
+/* const firstwordcapital  = firstname[0].toUpperCase() + firstname.slice(1);
+console.log(firstwordcapital) */
