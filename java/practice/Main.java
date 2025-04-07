8dcc1c3428995688767922c55e4be38a5f0dacb5
Karan <xkaranx5@gmail.com>
2025-04-07 13:39:21 +0530

    Remove Main class and test class files, reintroduce Main class in practice package

---
+++ java/practice/Main.java
@@ -0,0 +1,6 @@
+package practice;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello DSA in Java!");
+    }
+}
--- java/practice/test.java
+++ java/practice/test.java
@@ -1,4 +1,5 @@
-public class Main {
+package practice;
+final class test {
     public static void main(String[] args) {
         System.out.println("Hello DSA in Java!");
     }
